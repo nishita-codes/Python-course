41d766b1533f6cb8d8f23e7c6fa9b4e17706c591
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 14:32:45 +0530

    added files

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+
+
+# 1.  int() -----> convert to integer
+# 2. str() ------> convert to string
+# 3. float() -----> convert to float
+# 4. complex() ---> convert to complex number
+#  5. bool() ------> convert to boolean value
+
+
+a = 3
+b = 4
+print(str(a) + str(b))
+
+
+age = int(input("enter your age:"))
+print(age + 5)
