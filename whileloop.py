3125bb727bf8d071c058bf482dba83da2a8bbc33
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 23:20:05 +0530

    added files

---
+++ whileloop.py
@@ -0,0 +1,15 @@
+i=0
+while (i<3):
+    print(i)
+    i = i+1
+
+print("done with the loop")
+
+
+#EXAMPLE
+i= int(input("enter starting number:"))
+while(i<=38):
+    i = int(input("enter next number:"))
+    print(i)
+
+
