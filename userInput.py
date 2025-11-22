52663cacfed343960e5c00549ffa722c19d5c29a
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 14:37:19 +0530

    added files

---
+++ userInput.py
@@ -0,0 +1,10 @@
+# user input lene ke liye input() function use hota ha ----Yeh function always string return karta hai, chahe user number hi kyu na type kare.
+
+
+
+# SYNTAX:
+input("message")
+
+
+name = input("Enter your name:")
+print("hello " , name)
