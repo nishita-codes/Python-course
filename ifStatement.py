a3e8c580e6ab4fb4678d3cd39bde08596e1c6ffe
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 16:31:58 +0530

    added files

---
+++ ifStatement.py
@@ -0,0 +1,15 @@
+# if else conditional statement ka use decision lene me hota hai.
+
+# IF STATEMENT :
+age = 18
+if age>= 18:
+    print("you are eligible to vote")
+
+# IF ELSE STATEMENT:
+age = 17
+if age>= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote") 
+    
+    
