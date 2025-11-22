416f84f87d277e645d1719ed095f122a068f92b2
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-23 00:52:00 +0530

    added files

---
+++ functions.py
@@ -0,0 +1,17 @@
+# SYNTAX:
+# def function_name(parameters):
+#     #code
+#     return value
+
+
+def calculateGmean(x , y):
+    mean = (x*y) / (x+y)
+    print(mean)
+
+a = 5
+b=8
+calculateGmean(a,b)
+
+c = 9
+d =  2
+calculateGmean(c,d)
