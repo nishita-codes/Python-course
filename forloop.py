f5d7dd786318dab2bb48ea3f104441716b7521f2
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 22:31:22 +0530

    added files

---
+++ forloop.py
@@ -0,0 +1,22 @@
+
+# for loop with range
+for i in range(5):
+    print(i)
+
+
+# for loop with list
+fruits = ["apple", "banana", "cherry"]
+for f in fruits:
+    print(f)
+
+# for loop with string
+name = "nishita"
+for char in name:
+    print(char)
+
+# for loop with dictionary
+student = {"name": "nishita" , "age" : 21 , "city" : "delhi"}
+for key in student:
+    print(key ,":", student[key])     
+                
+
