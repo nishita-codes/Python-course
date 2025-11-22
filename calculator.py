c57b2f7ae70fea6dcbb42968a6b1929de721d902
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 14:17:29 +0530

    added files

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# CREATE A CALCULATOR CAPABLE OF PERFORMING ADDITION,SUBTRACTION,MULTIPLICATION AND DIVISION OPERATIONS ON TWO NUMBERS. Your program should format the output in a readable manner?
+
+
+a = 50
+b = 30
+
+print("The value of" , a ,"+" , b , "is: " , a+b)
+print("The value of" , a ,"-" , b , "is: " , a-b)
+print("The value of" , a ,"*" , b , "is: " , a*b)
+print("The value of" , a ,"/" , b , "is: " , a/b)
+print("The value of" , a ,"**" , b , "is: " , a**b)
