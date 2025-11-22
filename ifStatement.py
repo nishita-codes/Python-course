39bff9714f0ea263c61998d578e7daa41b85f958
Nishita-codes <nishitapatidar03@gmail.com>
2025-11-22 17:21:55 +0530

    added files

--- ifStatement.py
+++ ifStatement.py
@@ -31,3 +31,18 @@ else:
           print("number is positive")
     else:
        print("number is negative")
+
+
+
+    #    EXCERCISE:
+    # create a python program capable of greeting you with good moring, good afternoon and good evening. your program must use time module to get current hour. here is a sample program and documentation link for you.
+
+    import time
+    timestamp = time.strftime("%H:%M:%S")
+    print(timestamp)
+    timestamp = int(time.strftime("%H"))
+    print(timestamp)
+    timestamp = int(time.strftime("%M"))
+    print(timestamp)
+    timestamp = int(time.strftime("%S"))
+    print(timestamp)
